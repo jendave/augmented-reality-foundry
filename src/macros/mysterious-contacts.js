2ab5ff392c788b2e5f6559a4bc8a9049d3ef0517
David Hudson <jendave.hudson@gmail.com>
2023-11-15 01:11:57 -0800

    added last? macros

---
+++ src/macros/mysterious-contacts.js
@@ -0,0 +1,25 @@
+// Macro by David Hudson under the MIT License.
+
+function printMessage(message) {
+    let chatData = { content: message, };
+    ChatMessage.create(chatData, {})
+};
+
+try {
+    let rollTablesUUIDPrefix = "Compendium.augmented-reality-foundry.augmentedrealityrolltables.RollTable.";
+
+    let title = "<h3>50 Mysterious First Contacts</h3>";
+
+    const mysteriousContactsTable = await fromUuid(rollTablesUUIDPrefix + "iPt7lj912VWk1klW");
+    let mysteriousContactsRoll = await mysteriousContactsTable.roll();
+    let mysteriousContacts = mysteriousContactsRoll.results[0].text;
+
+    let message = mysteriousContacts;
+
+    // Print the message
+    printMessage(title + message);
+
+}
+catch (e) {
+    console.log(e);
+}
---
+++ src/macros/whats-on-the-river.js
@@ -0,0 +1,25 @@
+// Macro by David Hudson under the MIT License.
+
+function printMessage(message) {
+    let chatData = { content: message, };
+    ChatMessage.create(chatData, {})
+};
+
+try {
+    let rollTablesUUIDPrefix = "Compendium.augmented-reality-foundry.augmentedrealityrolltables.RollTable.";
+
+    let title = "<h3>What's On The River?</h3>";
+
+    const whatsOnTheRiverTable = await fromUuid(rollTablesUUIDPrefix + "KRIRlLWK8zoS0tFu");
+    let whatsOnTheRiverRoll = await whatsOnTheRiverTable.roll();
+    let whatsOnTheRiver = whatsOnTheRiverRoll.results[0].text;
+
+    let message = whatsOnTheRiver;
+
+    // Print the message
+    printMessage(title + message);
+
+}
+catch (e) {
+    console.log(e);
+}
